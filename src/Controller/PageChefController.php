98bc5faece00f4d53b034f6def3614d28ce34d31
Selkis18 <74144655+Selkis18@users.noreply.github.com>
2021-01-25 10:36:40 +0100

    debut affichage page chef par une boucle

--- src/Controller/PageChefController.php
+++ src/Controller/PageChefController.php
@@ -5,16 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\MembreRepository;
+use App\Entity\Membre;
 
 class PageChefController extends AbstractController
 {
     /**
      * @Route("/chef", name="page_chef")
      */
-    public function index(): Response
+    public function index(MembreRepository $membre): Response
     {
+        $page_chef = $membre->findAll();
         return $this->render('page_chef/index.html.twig', [
-            'controller_name' => 'PageChefController',
+            'membres' => $page_chef,
         ]);
     }
 }
