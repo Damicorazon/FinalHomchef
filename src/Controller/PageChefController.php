0b30ddf1f72d0115bb66ec4913d69cb4d4469001
Selkis18 <74144655+Selkis18@users.noreply.github.com>
2021-01-25 12:00:26 +0100

    boucle page des chefs en cours

--- src/Controller/PageChefController.php
+++ src/Controller/PageChefController.php
@@ -5,16 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Membre;
+use App\Repository\MembreRepository;
 
 class PageChefController extends AbstractController
 {
     /**
      * @Route("/chef", name="page_chef")
      */
-    public function index(): Response
+    public function index(MembreRepository $membre): Response
     {
+        $page_chef = $membre->findAll();
         return $this->render('page_chef/index.html.twig', [
-            'controller_name' => 'PageChefController',
+            'page_chef' => $page_chef,
+
         ]);
     }
 }
--- src/Controller/SpecialiteController.php
+++ src/Controller/SpecialiteController.php
@@ -5,16 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\MenuRepository;
 
 class SpecialiteController extends AbstractController
 {
     /**
      * @Route("/specialites", name="specialites")
      */
-    public function index(): Response
+    public function index(MenuRepository $menu): Response
     {
+        $liste_menu = $menu->findAll();
         return $this->render('specialite/index.html.twig', [
-            'controller_name' => 'SpecialiteController',
+            'menus' => $menu,
         ]);
     }
 }
