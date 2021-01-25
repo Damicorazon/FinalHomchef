532566c7c411f397dd3709475d071984325df6b5
Damdam <francis.munabeno@epitech.eu>
2021-01-25 13:44:08 +0100

    ajout boucle page specialites

--- src/Controller/SpecialiteController.php
+++ src/Controller/SpecialiteController.php
@@ -5,16 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\MenuRepository;
 
 class SpecialiteController extends AbstractController
 {
     /**
      * @Route("/specialites", name="specialites")
      */
-    public function index(): Response
+    public function index(menuRepository $menuRepository): Response
     {
+        $liste_menus = $menuRepository->findAll();
         return $this->render('specialite/index.html.twig', [
-            'controller_name' => 'SpecialiteController',
+            'menus' => $liste_menus,
         ]);
     }
 }
