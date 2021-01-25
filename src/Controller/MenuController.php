74766a6b0f8b324fdbef651f3b49253f97d5c477
Damdam <francis.munabeno@epitech.eu>
2021-01-25 15:52:32 +0100

    en cour formMenu

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use App\Entity\Menu;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\MenuRepository;
+use App\Entity\Menu;
+use App\Form\MenuType;
 
 class MenuController extends AbstractController
 {
@@ -34,4 +35,27 @@ class MenuController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/menu/ajouter", name="menu_ajouter")
+     */
+    public function nouveau(Request $request, EntityManagerInterface $em){
+        $menu = new Menu;
+        $formMenu = $this->createForm(MenuType::class, $menu);
+        $formMenu->handleRequest($request);
+        if( $formMenu->isSubmitted() && $formMenu->isValid() ){
+            if( $fichier = $formMenu->get("photos")->getData() ){
+                $destination = $this->getParameter("dossier_images");
+                $nomFichier = pathinfo($fichier->getClientOriginalName(), PATHINFO_FILENAME);
+                $nouveauNom = str_replace(" ", "_", $nomFichier);
+                $nouveauNom .= "_" . uniqid() . "." . $fichier->guessExtension();
+                $fichier->move($destination, $nouveauNom);
+                $menu->setCouverture($nouveauNom);
+            }
+            $em->persist($menu);
+            $em->flush();
+            $this->addFlash("success", "Le nouveau menu a bien été ajouté");
+            return $this->redirectToRoute("menu");
+        }
+        return $this->render("menu/ajouter.html.twig", ["formMenu" => $formMenu->createView()]);
+    }
 }
