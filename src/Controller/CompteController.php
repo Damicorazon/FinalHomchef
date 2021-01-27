7eaa912f61b7b27bffdda85b4ad5376021ff25e7
Selkis18 <74144655+Selkis18@users.noreply.github.com>
2021-01-27 16:08:43 +0100

    action modifier info des chefs ok

--- src/Controller/CompteController.php
+++ src/Controller/CompteController.php
@@ -7,6 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use App\Repository\MembreRepository;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Membre;
+use App\Form\MembreType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface as Encoder;
 
 /**
  * @IsGranted("ROLE_CHEF")
@@ -16,11 +20,11 @@ use App\Repository\MembreRepository;
 class CompteController extends AbstractController
 {
     /**
-     * @Route("/compte/fiche/{id}", name="compte")
+     * @Route("/compte", name="compte")
      */
-    public function index(MembreRepository $membre, $id): Response
+    public function index(MembreRepository $membre): Response
     {
-        $info_membre = $membre->find($id);
+        //$info_membre = $membre->find($id);
         $info_membre = $this->getUser();
         return $this->render('compte/index.html.twig', [
             'membre' => $info_membre,
@@ -36,5 +40,28 @@ class CompteController extends AbstractController
             'membre' => $description,
         ]);
     }
+    /**
+     * @Route("/compte/{id}/modifier", name="compte_modifier", methods={"GET","POST"})
+     */
 
+    public function edit(Request $request, Encoder $encoder, Membre $membre): Response
+    {
+        $formMembre = $this->createForm(MembreType::class, $membre);
+        $formMembre->handleRequest($request);
+
+        if ($formMembre->isSubmitted() && $formMembre->isValid()) {
+            $mdp = $formMembre->get("password")->getData();
+            if (trim($mdp)) {
+                $mdp = $encoder->encodePassword($membre, $mdp);
+                $membre->setPassword($mdp);
+            }
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('compte');
+        }
+        return $this->render('compte/modifier.html.twig', [
+            'membre' => $membre,
+            'formMembre' => $formMembre->createView(),
+        ]);
+    }
 }
