f540a36e02492389432f87c3456d91e02211914f
Selkis18 <74144655+Selkis18@users.noreply.github.com>
2021-01-28 16:56:11 +0100

    pb specialite page chef resolu + modif css

--- src/Controller/PageChefController.php
+++ src/Controller/PageChefController.php
@@ -13,14 +13,21 @@ use App\Entity\Menu;
 class PageChefController extends AbstractController
 {
     /**
-     * @Route("/{id}", name="page_chef", methods={"GET"}, requirements={"id"="\d+"})
+     * @Route("/page_chef/{id}", name="page_chef", methods={"GET"}, requirements={"id"="\d+"})
      */
     public function index(Membre $membre): Response
     {
-
+        $menus = $membre->getMenus();
+        //dd($menus);
+        $specialites = [];
+        foreach ($menus as $menu) {
+            $specialites[] = $menu->getSpecialite();
+        }
+        $specialites = array_unique($specialites);
+        //dd($specialites);
         return $this->render('page_chef/index.html.twig', [
             'membre' => $membre,
-
+            'specialites' => $specialites
         ]);
     }
 }
