ae23c3f234ec79dab358a6e68db2636899b0ec2a
Damdam <francis.munabeno@epitech.eu>
2021-01-28 10:00:52 +0100

    divers ajout/modif

--- src/Controller/PageChefController.php
+++ src/Controller/PageChefController.php
@@ -13,7 +13,7 @@ use App\Entity\Menu;
 class PageChefController extends AbstractController
 {
     /**
-     * @Route("/chef/{id}", name="page_chef", methods={"GET"}, requirements={"id"="\d+"})
+     * @Route("/{id}", name="page_chef", methods={"GET"}, requirements={"id"="\d+"})
      */
     public function index(Membre $membre): Response
     {
